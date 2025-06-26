361ae1656944a45acfb212df029f06665cd4a652
Venkat Gorla <venkatagorla@gmail.com>
2025-06-26 05:48:31 +0530

    [messages integration test] move query logic to separate file

---
+++ services/messaging-api/integration-test/ddb-messages-query.js
@@ -0,0 +1,51 @@
+import { DynamoDBClient, QueryCommand } from "@aws-sdk/client-dynamodb";
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
+import {
+  AWS_REGION,
+  MESSAGES_TABLE_NAME,
+  MESSAGES_GSI_NAME,
+} from "../constants.js";
+
+const ddb = new DynamoDBClient({ region: AWS_REGION });
+
+const fetchConversationsForUser = async (username) => {
+  const res = await ddb.send(
+    new QueryCommand({
+      TableName: MESSAGES_TABLE_NAME,
+      IndexName: MESSAGES_GSI_NAME,
+      KeyConditionExpression: "UserId = :u",
+      ExpressionAttributeValues: marshall({ ":u": `USER#${username}` }),
+    })
+  );
+
+  return res.Items.map(unmarshall);
+};
+
+const fetchMessagesForConversation = async (conversationId) => {
+  const res = await ddb.send(
+    new QueryCommand({
+      TableName: MESSAGES_TABLE_NAME,
+      KeyConditionExpression:
+        "ConversationId = :c AND begins_with(MessageSortKey, :msg)",
+      ExpressionAttributeValues: marshall({
+        ":c": conversationId,
+        ":msg": "MSG#",
+      }),
+    })
+  );
+  return res.Items.map(unmarshall);
+};
+
+(async () => {
+  console.log("\nFetching conversations for user: alice\n");
+  const convos = await fetchConversationsForUser("alice");
+  for (const convo of convos) {
+    console.log("Convo:", convo.ConversationIndex);
+    const messages = await fetchMessagesForConversation(
+      convo.ConversationIndex
+    );
+    messages.forEach((m) => {
+      console.log(` - [${m.Sender}] ${m.Message}`);
+    });
+  }
+})();
