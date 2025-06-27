a8ed29c5f2641c2cc9548141b41c48fa27a5b9e0
Venkat Gorla <venkatagorla@gmail.com>
2025-06-27 05:56:28 +0530

    [react client] bootstrap integration

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div className="container py-5">
+      <h1 className="text-info">Bootstrap is working 🎉</h1>
+    </div>
   );
 }
 
