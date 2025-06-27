8dd3358fed9a311ed8915d9c97c7499494789142
Venkat Gorla <venkatagorla@gmail.com>
2025-06-27 15:09:55 +0530

    [react nonfunctional] remove Chat list commented code

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -1,22 +1,3 @@
-// import ChatList from "./components/ChatList";
-
-// function App() {
-//   return (
-//     <div className="container-fluid vh-100">
-//       <div className="row h-100">
-//         <div className="col-3 border-end p-0 overflow-auto">
-//           <ChatList />
-//         </div>
-//         <div className="col-9 p-3">
-//           <h2>Chat Panel</h2>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import AppRoutes from "./AppRoutes";
