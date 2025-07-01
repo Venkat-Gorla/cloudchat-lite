d5be2d2079b19b6ae097c0ff5337b6bfa85f7cd1
Venkat Gorla <venkatagorla@gmail.com>
2025-07-01 16:33:22 +0530

    [Lambda lib] utility function for id token verification and decoding - integration test pending

--- services/messaging-api/utils/auth-lib.js
+++ services/messaging-api/utils/auth-lib.js
@@ -1,4 +1,7 @@
 import { AdminInitiateAuthCommand } from "@aws-sdk/client-cognito-identity-provider";
+import jwt from "jsonwebtoken";
+import jwksRsa from "jwks-rsa";
+import { AWS_REGION } from "../constants.js";
 
 export function createAuthCommand(event) {
   const clientId = process.env.COGNITO_CLIENT_ID;
@@ -27,5 +30,38 @@ export function createAuthCommand(event) {
       USERNAME: username,
       PASSWORD: password,
     },
+  });
+}
+
+const USER_POOL_ID = process.env.COGNITO_USER_POOL_ID;
+const ISSUER = `https://cognito-idp.${AWS_REGION}.amazonaws.com/${USER_POOL_ID}`;
+
+const client = jwksRsa.createRemoteJWKSet(
+  new URL(`${ISSUER}/.well-known/jwks.json`)
+);
+
+export async function verifyToken(token) {
+  const { header } = jwt.decode(token, { complete: true }) || {};
+  if (!header?.kid) {
+    throw new Error("Token header missing `kid`");
+  }
+
+  const key = await client({ kid: header.kid });
+
+  return new Promise((resolve, reject) => {
+    jwt.verify(
+      token,
+      key,
+      {
+        issuer: ISSUER,
+        algorithms: ["RS256"],
+      },
+      (err, decoded) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(decoded);
+      }
+    );
   });
 }
